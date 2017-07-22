730045fc2874fae49c88bf680c0973ff9d2e10cf
phuonghqh <phuonghqh@gmail.com>
2017-07-22 13:35:32 +0700

    Correct description of trigger in project page

--- src/main/java/com/ribose/jenkins/plugin/awscodecommittrigger/net/SQSChannelImpl.java
+++ src/main/java/com/ribose/jenkins/plugin/awscodecommittrigger/net/SQSChannelImpl.java
@@ -49,7 +49,7 @@ public class SQSChannelImpl implements SQSChannel {
 
     @Override
     public List<Message> getMessages() {
-        log.info("Poll for messages");
+        log.info("Star polling messages");
 
         List<Message> messages = Collections.emptyList();
 
